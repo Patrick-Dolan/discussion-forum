af43a418bf62f132686c751f1d85e1c0b908d1e6
Patrick Dolan <dolanp1992@gmail.com>
2022-06-07 20:23:40 -0700

    setup redux store and reducer
    Co-authored-by: Nick Sullivan <nicholassull@users.noreply.github.com>
    Co-authored-by: chrisdfghjkl <chrisdfghjkl@users.noreply.github.com>
    Co-authored-by: marnionrails <marnionrails@users.noreply.github.com>

---
+++ src/reducers/post-list-reducer.js
@@ -0,0 +1,21 @@
+export default (state = {}, action) => {
+  const {title, author, date, postBody, id} = action;
+  switch (action.type) {
+    case 'ADD_POST':
+    return Object.assign({}, state, {
+      [id]: {
+        title: title,
+        author: author,
+        date: date,
+        postBody: postBody,
+        id: id
+      }
+    });
+    case 'DELETE_POST':
+      let newState = {...state};
+      delete newState[id];
+      return newState;
+    default:
+      return state;
+  }
+};
